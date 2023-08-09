bf1052b15a3ac25f5e0d3630694952b42462d5cd
qibao <ufofcz@163.com>
2023-08-10 00:58:28 +0800

    update test llvm

---
+++ 90_llvm_first/build.sh
@@ -0,0 +1,2 @@
+
+# https://blog.csdn.net/weixin_43296779/article/details/121989541?spm=1001.2101.3001.6650.6&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-6-121989541-blog-120827879.235%5Ev38%5Epc_relevant_default_base3&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-6-121989541-blog-120827879.235%5Ev38%5Epc_relevant_default_base3&utm_relevant_index=10
